f73f7c742b3d05fcc33d204cb62146dedfc0fa9a
e-a-w- <eawatkins.0@gmail.com>
2020-08-01 17:49:40 -0400

    making api key and search

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SearchNav from './SearchNav'
 import TopicNav from './TopicNav'
 import HomeBody from './HomeBody'
 
-
 const HomePage = () => {
+    
+    const [searchTerm, setSearchTerm] = useState("");
+    const [results, setResults] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        fetch(`/api/search?searchQuery=${searchTerm}`, {
+            method: 'get',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then((res) => res.json())
+            .then((res) => {
+              console.log(res);
+              setResults(res);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+    }
+
     return (
         <>
             <TopicNav />
-            <SearchNav />
+            <SearchNav handleSubmit={handleSubmit} setSearchTerm={setSearchTerm} />
             <HomeBody />
         </>
     )
