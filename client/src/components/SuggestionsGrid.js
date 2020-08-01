3d8f8fb903f60898664e1f2cb8c38116c7690a80
e-a-w- <eawatkins.0@gmail.com>
2020-08-01 15:37:08 -0400

    Suggestions Grid commit

--- client/src/components/SuggestionsGrid.js
+++ client/src/components/SuggestionsGrid.js
@@ -1,11 +1,32 @@
 import React from 'react'
-import SearchNav from './SearchNav'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
 
 const SuggestionsGrid = () => {
+
+    const cardStyle = {
+        width: "200px",
+        margin: "10px"
+    }
+
+    const containerStyle = {
+        marginTop: "40px",
+        display: "flex",
+        flexWrap: "wrap"
+    }
+
     return (
-        <>
-            STUFF
-        </>
+        <Container style={containerStyle}>
+            <Card style={cardStyle}>
+                <Card.Body>
+                    <Card.Img variant="top" src="http://placehold.jp/99ccff/003366/100x100.png" />
+                </Card.Body>
+                <Card.Footer>
+                    <Button style={{width: "100%"}} variant="secondary">Link</Button>
+                </Card.Footer>
+            </Card>
+        </Container>
     )
 }
 
