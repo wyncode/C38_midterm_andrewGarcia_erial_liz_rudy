05e06cec5a44187360298f209d72e40ce278b1b5
rrudy0513 <rrudy0513@gmail.com>
2020-08-01 17:10:32 -0400

    fix Eouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,18 @@ import ErrorPage from './components/ErrorPage'
 import MemePage from './components/MemePage'
 import HomePage from './components/HomePage'
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
   return (
-    // <ErrorPage />
-    <HomePage />
-    // <MemePage />
-  );
+    
+    <Router> 
+      <Switch>
+        <Route path="./MemePage.js" component={MemePage} />
+        <HomePage />
+        <Route path="./HomePage.js" component={HomePage} />
+      </Switch>
+      </Router>
+  )
 };
 
 export default App;
