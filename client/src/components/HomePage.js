ea46fadae97fa0c338eef52a7826c53b257e7550
rrudy0513 <rrudy0513@gmail.com>
2020-08-01 15:50:51 -0400

    add rotuer and switch to the homepage.js

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,15 +2,27 @@ import React from 'react'
 import SearchNav from './SearchNav'
 import TopicNav from './TopicNav'
 import HomeBody from './HomeBody'
+import MemePage from './MemePage'
+import ErroePage from './ErrorPage'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const HomePage = () => {
     return (
-        <>
-            <TopicNav />
-            <SearchNav />
+
+          <Router>
+          <TopicNav />
+          <SearchNav />
+            <Switch>
+              <Route path="./MemePage.js" component={MemePage} />
+              <Route path="./ErrorPage.js" component={ErroePage} />
+              <Route path="./HomePage.js" component={HomePage} />
+            </Switch>
             <HomeBody />
-        </>
-    )
+        </Router>
+      );
+
+
+    
 }
 
 export default HomePage
