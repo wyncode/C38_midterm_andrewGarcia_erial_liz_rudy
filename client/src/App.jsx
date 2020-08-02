fa910cd4a45abedeee96c73e7f774d4ee7d1b15a
e-a-w- <eawatkins.0@gmail.com>
2020-08-01 20:05:14 -0400

    added search pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,21 @@ import React, { useState, useEffect } from 'react';
 import ErrorPage from './components/ErrorPage'
 import MemePage from './components/MemePage'
 import HomePage from './components/HomePage'
+import SearchPage from './components/SearchPage'
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
   return (
     
-    <Router> 
-      <Switch>
-        <Route path="./MemePage.js" component={MemePage} />
-        <HomePage />
-        <Route path="./HomePage.js" component={HomePage} />
-      </Switch>
-      </Router>
+    // <Router> 
+    //   <Switch>
+    //     <Route path="./MemePage.js" component={MemePage} />
+    //     <HomePage />
+    //     <Route path="./HomePage.js" component={HomePage} />
+    //   </Switch>
+    //   </Router>
+
+    <SearchPage />
   )
 };
 
---
+++ client/src/components/SearchPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchNav from './SearchNav'
+import SearchBody from './SearchBody'
+
+const SearchPage = () => {
+    return (
+        <>
+            <SearchNav />
+            <SearchBody />
+        </>
+    )
+}
+
+export default SearchPage
