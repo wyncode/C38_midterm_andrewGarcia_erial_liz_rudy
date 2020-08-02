547ea2dd0c16a66ed6de2bcef4206771fb38c098
Liz <eawatkins.0@gmail.com>
2020-08-02 08:47:24 -0400

    added search pages (#18)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ErrorPage from './components/ErrorPage'
 import MemePage from './components/MemePage'
 import HomePage from './components/HomePage'
+import SearchPage from './components/SearchPage'
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
---
+++ client/src/components/SearchBody.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import TopicCard from './TopicCard'
+
+
+const SearchBody = () => {
+    return (
+        <Container>
+        <Row>
+            <Col>
+                
+            <TopicCard />
+
+
+            </Col>
+        </Row>
+    </Container>
+    )
+}
+
+export default SearchBody
