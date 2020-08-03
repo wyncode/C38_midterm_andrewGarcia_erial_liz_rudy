e02075791473d89ee515d50fcae832ea0a81b20c
garandrew102 <garandrew102@gmail.com>
2020-08-03 17:44:19 -0400

    meme search result body

--- client/src/components/MemeBody.js
+++ client/src/components/MemeBody.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MemeCard from './MemeCard';
 import SideLinks from './SideLinks';
-
+import RandomCard from './RandomCard';
+import Alert from 'react-bootstrap/Alert';
 
 const MemeBody = ({meme}) => {
+
+  const [randomMeme, setRandomMeme] = useState([]);
+
+  useEffect(() => {
+        fetch('/api/random', {
+          method: 'get',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          }})
+      .then(res => res.json())
+      .then(data => {
+     console.log(data);
+        setRandomMeme(data);
+      })
+      .catch(error => console.error(error))
+  }, []);
+
   return (
     <Container fluid>
       <Row>
@@ -16,6 +35,18 @@ const MemeBody = ({meme}) => {
         </Col>
         <Col sm={12} md={3}>
           <SideLinks />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Alert variant='secondary' style={{width:'80vw', margin:'100px auto 40px', textAlign:'center'}}>More Random Memes for Your Entertainment!</Alert>
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{display:'flex', flexWrap:'wrap', justifyContent:'center'}}>
+        {randomMeme.map(item => {
+               return <RandomCard key={item.id} id={item.id} title={item.title} imgUrl={item.img_url}  alt="Gif"/>
+            })}
         </Col>
       </Row>
     </Container>
---
+++ client/src/components/RandomCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const RandomCard = ({ id, title, imgUrl }) => {
+  const history = useHistory();
+
+  const handleNavigate = () => {
+    history.push(`/meme/${id}`);
+  };
+
+  return (
+    <Card style={{ width: '300px', margin: "10px"}}>
+      <Card.Body>
+        <Card.Img variant="top" src={imgUrl}  alt="Gif"/>
+      </Card.Body>
+      <Card.Footer>
+        <Button
+          onClick={handleNavigate}
+          style={{ width: '100%' }}
+          variant="secondary"
+        >
+          {title}
+        </Button>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default RandomCard;
