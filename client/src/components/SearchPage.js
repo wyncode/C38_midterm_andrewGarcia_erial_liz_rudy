02b2feed71b87fa00f69f2f088d37dac2266c107
Erialbania <elope217@fiu.edu>
2020-08-03 16:15:09 -0400

    search results title

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -3,10 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import TopicCard from './TopicCard';
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 const SearchPage = ({ results }) => {
   return (
     <Container>
+      <Jumbotron>
+              <h1>Meme Results...</h1>
+          </Jumbotron>
       <Row>
         <Col style={{display:"flex", flexWrap:"wrap"}}>
           {results.map((gif) => (
