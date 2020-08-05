b633ab960c96a381fbf183237e75ca711a900afb
Erialbania <61883789+Erialbania@users.noreply.github.com>
2020-08-05 15:09:47 -0400

    final edits (#62)

--- client/src/components/SideLinks.js
+++ client/src/components/SideLinks.js
@@ -21,31 +21,39 @@ const SideLinks = ({ meme }) => {
           Back
         </Button>
       </div>
+      <div id="view-ctr">
+        <img
+          src="https://www.webfreecounter.com/hit.php?id=gmufdfcn&nd=6&style=11"
+          border="0"
+          alt="visitor counter"
+        />{' '}
+        &nbsp; Views
+      </div>
       {meme && (
         <div>
           <div id="socialbuttons">
-            <TwitterShareButton url={`/meme/${meme.id}`} className="spin">
+            <TwitterShareButton
+              url={`https://murmuring-ravine-09544.herokuapp.com/meme/${meme.id}`}
+              className="spin"
+            >
               <TwitterIcon size={32} round={true} />
             </TwitterShareButton>
-            <TumblrShareButton url={`/meme/${meme.id}`} className="spin">
+            <TumblrShareButton
+              url={`https://murmuring-ravine-09544.herokuapp.com/meme/${meme.id}`}
+              className="spin"
+            >
               <TumblrIcon size={32} round={true} />
             </TumblrShareButton>
-            <FacebookShareButton url={`/meme/${meme.id}`} className="spin">
+            <FacebookShareButton
+              url={`https://murmuring-ravine-09544.herokuapp.com/meme/${meme.id}`}
+              className="spin"
+            >
               <FacebookIcon size={32} round={true} />
             </FacebookShareButton>
           </div>
         </div>
       )}
-      <div>
-        <div id="view-ctr">
-          <img
-            src="https://www.webfreecounter.com/hit.php?id=gmufdfcn&nd=6&style=11"
-            border="0"
-            alt="visitor counter"
-          />{' '}
-          &nbsp; Views
-        </div>
-      </div>
+      <div></div>
     </div>
   );
 };
