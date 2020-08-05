1e982cdfb27f5be926163753e21a32d58ae19514
Erialbania <elope217@fiu.edu>
2020-08-05 14:14:36 -0400

    final edits

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Credit from './components/Credit'
+import Credit from './components/Credit';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/style.css';
 
-ReactDOM.render([<App key="1" />, <Credit key="2" />], document.getElementById('root'));
+ReactDOM.render(
+  [<App key="1" />, <Credit key="2" />],
+  document.getElementById('root')
+);
